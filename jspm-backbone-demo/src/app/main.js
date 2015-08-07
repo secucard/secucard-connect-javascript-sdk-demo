ad7f1afd1d9d66c30c734720eafda3f167973460
Valiantsin Luchko <akmeful@gmail.com>
2015-08-07 16:34:04 +0300

    update jspm demo

--- jspm-backbone-demo/src/app/main.js
+++ jspm-backbone-demo/src/app/main.js
@@ -3,23 +3,21 @@ import {Action} from './action';
 import {Result} from './result';
 import {Events} from './events';
 //import {Config} from './config';
-import {SecucardConnect} from 'secucard-connect';
+import {SecucardConnect, Services} from 'secucard-connect';
 import {Uuid} from './uuid';
 import {Net} from './net';
 import {Urls} from './urls';
 
-console.log(SecucardConnect);
-
 
 var onStartDemo = function () {
-	var self = this
+	var self = this;
 	$('#start-demo').click(function () {
 		$(this).fadeOut()
 		self.events.vent.trigger('action:start', function () {
 			$('#demo').fadeIn()
 		})
 	})
-}
+};
 var wireUpAjaxLoader = () => {
 	$(document)
 		.ajaxStart(() => {
@@ -28,32 +26,8 @@ var wireUpAjaxLoader = () => {
 		.ajaxStop(() => {
 			$("#ajax-spinner-modal").modal('hide');
 		});
-}
-var configFor = (request) => {
-	return Config.stomp.requests[request]
-}
-var initEvents = function () {
-	var self = this;
-	
-	self.events.commands.setHandler("makeStompCall", () => {
-		var call = self.events.reqres.request("stomp-call-type")
-		var accessToken = self.accessToken
-		var stompListener = function (event, details) {
-			var message = event == "error" ? details.toString() : details
-			self.events.vent.trigger("results:update:stomp:call:view", event, message)
-		}
-		var config = configFor(call)
-		var params = {
-			accessToken: accessToken,
-			requestId: self.uuidGen.uuid(),
-			requestMethod: config.method,
-			options: config.options
-		}
-		self.stomp
-			.listener(stompListener)
-			.request(params)
-	})
-}
+};
+
 export class Application {
 	
 	init() {
@@ -67,12 +41,7 @@ export class Application {
 	
 	initSecucard() {
 		
-		let config = {
-			stompHost: 'connect-dev10.secupay-ag.de'
-		};
-		
-		this.client = SecucardConnect.create(config);
-		this.stomp = this.client.context.getStompChannel();
+		this.client = SecucardConnect.create();
 		
 	}
 	
@@ -80,9 +49,28 @@ export class Application {
 
 		this.events.vent.on("results:done:get:client:credentials", (token) => {
 			this.updateClientToken(token);
-			this.stomp.open().then(()=>{
-				console.log('Stomp opened');
+		});
+		
+		this.events.vent.on("results:show:make:stomp:call:view", (token) => {
+			this.client.open().then(()=>{
+				console.log('opened');
+				$('#btn-make-stomp-call').prop('disabled', false);
+			}).catch((err)=>{
+				console.log(err.message);
 			});
+		});
+		
+		this.events.commands.setHandler("makeStompCall", () => {
+			
+			let skeletons = this.client.getService(Services.General.Skeletons);
+			skeletons.retrieveList().then((res) => {
+				console.log(res);
+				this.events.vent.trigger('results:update:stomp:call:view', 'message', JSON.stringify(res));
+			}).catch((err) => {
+				console.log(err);
+				this.events.vent.trigger('results:update:stomp:call:view', 'error', JSON.stringify(err));
+			});
+			
 		});
 		
 	}
@@ -98,8 +86,8 @@ export class Application {
 $(() => {
 	var app = new Application();
 	app.init().done(function (response) {
-		app.uuidGen = new Uuid(response.uuid)
-		wireUpAjaxLoader()
+		app.uuidGen = new Uuid(response.uuid);
+		wireUpAjaxLoader();
 		onStartDemo.call(app)
 	})
 });
