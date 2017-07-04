ec0eb2ec08d630b71d1fc78bf61c0fb8ae2d1b7b
Rico Simlinger <r.simlinger@secupay.ag>
2017-07-04 16:09:33 +0200

    ADD sample code for product payment

--- node-demo/01_client_simple/index.js
+++ node-demo/01_client_simple/index.js
@@ -3,15 +3,15 @@ var Services = require('secucard-connect').Services;
 
 // set credentials
 var credentials = {
-    "client_id": "09ae83af7c37121b2de929b211bad944",
-    "client_secret": "9c5f250b69f6436cb38fd780349bc00810d8d5051d3dcf821e428f65a32724bd"
+  'client_id': '09ae83af7c37121b2de929b211bad944',
+  'client_secret': '9c5f250b69f6436cb38fd780349bc00810d8d5051d3dcf821e428f65a32724bd',
 };
 
 var config = {
   restUrl: 'https://connect-testing.secupay-ag.de/api/v2/',
   oAuthUrl: 'https://connect-testing.secupay-ag.de/oauth/',
   stompHost: 'connect-testing.secupay-ag.de',
-  stompEnabled: true
+  stompEnabled: true,
 };
 
 // create Secucard client
@@ -19,10 +19,10 @@ var client = SecucardConnect.create(config);
 client.setCredentials(credentials);
 
 // establish connection
-client.open().then(function(){
-    console.log("Secucard client connected");
-}).catch(function(err){
-	console.log(err);
+client.open().then(function() {
+  console.log('Secucard client connected');
+}).catch(function(err) {
+  console.log(err);
 });
 
 var Hapi = require('hapi');
@@ -30,18 +30,37 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({
-	host: 'localhost',
-	port: 9000
+  host: 'localhost',
+  port: 9000,
 });
 
 server.route({
-	method: 'GET',
-	path: '/hello',
-	handler: function (request, reply) {
-		reply('hello world');
-	}
+  method: 'GET',
+  path: '/hello',
+  handler: function(request, reply) {
+    reply('hello world');
+  },
 });
 
-server.start(function () {
-	console.log('Running on 9000');
+server.route({
+  method: 'GET',
+  path: '/get_customers',
+  handler: function(request, reply) {
+    var customers = client.getService(Services.Payment.Customers);
+
+    var queryParams = {
+      count: 3,
+      offset: 0
+    };
+
+    customers.retrieveList(queryParams).then(function(res){
+      reply(res);
+    }).catch(function(err){
+      console.log(err);
+    });
+  }
+});
+
+server.start(function() {
+  console.log('Running on 9000');
 });
